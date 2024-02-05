1930f8a4bcea55da410675f30a47974a279bc829
neslihan <neslihan_atasvr111@outlook.com>
2024-02-06 01:25:55 +0300

    started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue: "#14B1B9",
+        darkblue: "#101828",
+        darkgray: "#667085",
+        lightgray: "#A2ABBA",
+        white: "#FFFFFF",
+      },
     },
   },
   plugins: [],
