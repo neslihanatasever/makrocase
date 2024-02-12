7f4a539df6a2101589cabdac35ee2d391e8bc430
neslihan <neslihan_atasvr111@outlook.com>
2024-02-12 04:28:47 +0300

    ödeme sayfasının tasarımı yapıldı

--- app/pages/campaign/page.tsx
+++ app/pages/campaign/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function CampaignPage() {
     return(
-        <main>
-        <div className="w-4/5 h-full flex flex-col ml-40 border-solid border-2 rounded-3xl mt-10 mb-10">
+        <main className="container mx-auto">
+        <div className="w-full h-full flex flex-col border-solid border-2 rounded-3xl mt-10 px-5 mb-10">
           <div className="m-5 flex flex-col">
             <label>Senin için uygun olan paketi seç</label>
             <label className="text-2xl text-xl">Tahmini başlangıç tarihini seç</label>
           </div>
   
-          <div className="w-4/5 h-25 flex flex-col border-solid border-2 rounded-3xl p-2 ml-5 mb-5">
+          <div className="w-full h-25 flex flex-col border-solid border-2 rounded-3xl p-2 mb-5">
             <label className="ml-5 mb-3 text-blue">Başlangıç tarihini öne çek</label>
             <label className="ml-5 font-semibold">4 Nisan - 6 Nisan 2023</label>
   
@@ -20,7 +20,7 @@ export default function CampaignPage() {
               <label className="mr-3">$5299</label>
             </div>
           </div>
-          <div className="w-4/5 h-25 flex flex-col border-solid border-2 rounded-3xl p-2 ml-5 mb-5">
+          <div className="w-full h-25 flex flex-col border-solid border-2 rounded-3xl p-2  mb-5">
             <label className="ml-5 mb-3 text-blue">Varsayılan sıraya gir</label>
             <label className="ml-5 font-semibold">23 Nisan - 25 Nisan 2023</label>
   
@@ -35,7 +35,7 @@ export default function CampaignPage() {
         </div>
         <div className="mb-10">
         <div>
-          <div className="flex justify-end items-center mr-40 mt-10 gap-3">
+          <div className="flex justify-end items-center mt-10 gap-3">
             <Link href={"/pages/package"}>
               <button className="bg-buttongray w-40 h-10 rounded-xl">
                 Geri Dön
--- app/pages/pay/page.tsx
+++ app/pages/pay/page.tsx
@@ -1,5 +1,94 @@
 export default function PayPage() {
-    return(
-        <div>hellooooğğğ kampanyağğ</div>
-    )
+  return (
+    <main className="container mx-auto mt-10 px-5 mb-10">
+      <div className="text-3xl text-bold p-5">
+        <h1>Onayla ve ödemeye geç</h1>
+      </div>
+      <hr />
+      <div className="flex gap-10 mt-5">
+        <div className="w-[60%] h-full flex flex-col">
+          <div className="text-xl text-bold p-5">
+            <h2>Kampanya özetin</h2>
+          </div>
+
+          <div className="flex flex-col  gap-5">
+            <div className="flex justify-between">
+              <div className="flex flex-col mb-2">
+                <label className="ml-5 mb-3 text-lightgray">Tıklanma</label>
+                <label className="ml-5 font-semibold">10.000 Tıklanma</label>
+              </div>
+              <div>
+                <label className="ml-5 font-semibold text-lightgray">
+                  Düzenle
+                </label>
+              </div>
+            </div>
+
+            <div className=" flex justify-between">
+              <div className="flex flex-col  ">
+                <label className="ml-5 mb-3 text-lightgray">
+                  Başlangıç Tarihi
+                </label>
+                <label className="ml-5 font-semibold">
+                  4 Nisan - 6 Nisan 2023
+                </label>
+              </div>
+              <div>
+                <label className="ml-5 font-semibold text-lightgray">
+                  Düzenle
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[40%]  border-solid border-2 rounded-3xl">
+          <div className="p-5 text-lightgray text-md ">
+            <h3>Kampanya Özetin</h3>
+          </div>
+          <div className="mb-5">
+            <hr />
+          </div>
+          <div className="border-solid border-2 rounded-3xl flex justify-between mr-7 ml-7">
+            <img src="" alt="" className="w-[20px] h-[20px] rounded-lg" />
+            <div className="">
+              <p className="text-bold text-md font-semibold">
+                Kaygılarım kutsal kitap gibi
+              </p>
+              <p className="text-lightgray">kurkafanda</p>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className="ml-7 mt-7">
+            <label className=" font-semibold text-xl">Ödeme Detayları</label>
+          </div>
+
+          <div className=" flex justify-between m-7 ">
+            <div className="flex flex-col  ">
+              <label className="">Başlangıç Tarihi</label>
+            </div>
+            <div>
+              <label className=" font-semibold">2399$</label>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className=" flex justify-between m-7 mb-[-2px]">
+            <div className="flex flex-col  ">
+              <label className=" mb-3">Başlangıç Tarihi</label>
+            </div>
+            <div>
+              <label className=" font-semibold ">2399$</label>
+            </div>
+          </div>
+
+          <button className="bg-blue w-[30vw] h-10 rounded-xl text-white ml-6 mt-6 mb-5">
+            Ödemeye Geç
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
